c1c1230c487db82a1c06f5f33bdbc410711a1142
larks11 <lark.alunan@gmail.com>
2026-03-20 23:53:52 +0800

    fix

--- frontend/src/components/Paginate.jsx
+++ frontend/src/components/Paginate.jsx
@@ -1,22 +1,26 @@
 import { Pagination } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 const Paginate = ({ pages, page, isAdmin = false, keyword = '' }) => {
+  const navigate = useNavigate();
+
   return (
     pages > 1 && (
       <Pagination>
         {[...Array(pages).keys()].map((x) => (
           <Pagination.Item
-            as={Link}
             key={x + 1}
-            to={
-              !isAdmin
-                ? keyword
-                  ? `/search/${keyword}/page/${x + 1}`
-                  : `/page/${x + 1}`
-                : `/admin/productlist/${x + 1}`
-            }
             active={x + 1 === page}
+            onClick={() =>
+              navigate(
+                !isAdmin
+                  ? keyword
+                    ? `/search/${keyword}/page/${x + 1}`
+                    : `/page/${x + 1}`
+                  : `/admin/productlist/${x + 1}`
+              )
+            }
+            style={{ cursor: 'pointer' }}
           >
             {x + 1}
           </Pagination.Item>
