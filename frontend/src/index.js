25a3d2154830a488689b67335265ef654390fc70
larks11 <lark.alunan@gmail.com>
2026-03-20 22:30:21 +0800

    hardcode google client id test

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -81,7 +81,6 @@ root.render(
     <HelmetProvider>
       <Provider store={store}>
         <PayPalScriptProvider deferLoading={true}>
-          {/* GOOGLE OAUTH PROVIDER — i-replace ang YOUR_GOOGLE_CLIENT_ID */}
           <GoogleOAuthProvider clientId="28291555489-3ojl3sp2flaf7iplgmgi7kjgmjensbor.apps.googleusercontent.com">
             <RouterProvider router={router} />
           </GoogleOAuthProvider>
